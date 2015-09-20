94914178d3735a236c74cce450f4cfb16c42dd23
Hellstern <tuhe@cphbusiness.dk>
2015-09-20 17:33:03 +0200

    Nyt navn

---
+++ JDBC Exercise/src/jdbc/exercise/soloution/DB.java
@@ -0,0 +1,8 @@
+package jdbc.exercise.soloution;
+
+public class DB {
+	public static String driver = "com.mysql.jdbc.Driver";
+	public static String URL    = "jdbc:mysql://localhost/JDBCExerise";
+	public static String ID     = "admin";
+	public static String PW     = "sql1234";
+}
